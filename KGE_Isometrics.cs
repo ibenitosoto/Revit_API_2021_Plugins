2859bdb38a9b1263f178c67dba66ea5cdc1f4d56
ibenitosoto <ignaciobenitosoto@gmail.com>
2022-04-21 20:04:54 +0100

    Add project files.

---
+++ KGE_Isometrics.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB.Plumbing;
+
+namespace API_2021_Plugins
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class KGE_Isometrics : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UI Document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+
+            //Create Filters
+            ElementCategoryFilter pipesFilter = new ElementCategoryFilter(BuiltInCategory.OST_PipeCurves);
+            ElementCategoryFilter fittingsFilter = new ElementCategoryFilter(BuiltInCategory.OST_PipeFitting);
+            ElementCategoryFilter valvesFilter = new ElementCategoryFilter(BuiltInCategory.OST_PipeAccessory);
+
+            //Multicategory filter
+            //Initialize list inheriting from IList and adding all 3 categories
+            IList<BuiltInCategory> allCategories = new List<BuiltInCategory>();
+            allCategories.Add(BuiltInCategory.OST_PipeCurves);
+            allCategories.Add(BuiltInCategory.OST_PipeFitting);
+            allCategories.Add(BuiltInCategory.OST_PipeAccessory);
+
+            //Creating the multicategory filter
+            ElementMulticategoryFilter allElementsFilter = new ElementMulticategoryFilter(allCategories);
+
+            //Collect elements separately
+            IList<Element> pipes = new FilteredElementCollector(doc).WherePasses(pipesFilter).WhereElementIsNotElementType().ToElements();
+            IList<Element> fittings = new FilteredElementCollector(doc).WherePasses(fittingsFilter).WhereElementIsNotElementType().ToElements();
+            IList<Element> valves = new FilteredElementCollector(doc).WherePasses(valvesFilter).WhereElementIsNotElementType().ToElements();
+
+            //Collect all elements together
+            IList<Element> allElements = new List<Element>();
+            allElements = new FilteredElementCollector(doc).WherePasses(allElementsFilter).WhereElementIsNotElementType().ToElements();
+
+            TaskDialog.Show("Welcome", "Welcome to the KGE Isometrics plugin");
+            TaskDialog.Show("Quantifications", $"{pipes.Count} pipes in the model\n" +
+                                                $"{fittings.Count} fittings in the model\n" +
+                                                $"{valves.Count} valves in the model\n");
+
+
+
+
+            var lineNumberGroups = from e in allElements
+                                   //group e by e.GetParameters("EZ_LINE NO").ToString();
+                                   group e by e.LookupParameter("EZ_LINE NO").AsString();
+
+            foreach (var lineNumberGroup in lineNumberGroups)
+            {
+                //TaskDialog.Show("Line Number Groups", $"Line number {lineNumberGroup.Key} found\n" +
+                //    $"Number of elements with that line number: {lineNumberGroup.Count()}");
+
+                List<ElementId> elementIDs = new List<ElementId>();
+
+                foreach (Element e in lineNumberGroup)
+                {
+                    //TaskDialog.Show("Category", $"Category of element is {e.Category.Name}");
+                    elementIDs.Add(e.Id);
+                }
+
+
+                using (Transaction transaction = new Transaction(doc))
+                {
+                    ElementId categoryId = lineNumberGroup.First().Category.Id;
+                    transaction.Start("Create Assembly Instance");
+                    AssemblyInstance assemblyInstance = AssemblyInstance.Create(doc, elementIDs, categoryId);
+                    transaction.Commit(); // need to commit the transaction to complete the creation of the assembly instance so it can be accessed in the code below
+
+                    string assemblyName = lineNumberGroup.Key.ToString();
+
+                    if (transaction.GetStatus() == TransactionStatus.Committed)
+                    {
+                        transaction.Start("Set Assembly Name");
+                        if (assemblyName.Length > 0)
+                        {
+                            assemblyInstance.AssemblyTypeName = assemblyName;
+                        }
+
+                        else
+                        {
+                            assemblyInstance.AssemblyTypeName = "naming_error";
+                        }
+                        
+                        transaction.Commit();
+
+                    }
+
+
+
+                    if (assemblyInstance.AllowsAssemblyViewCreation()) // check to see if views can be created for this assembly
+                    {
+
+                        if (transaction.GetStatus() == TransactionStatus.Committed)
+                        {
+
+                            transaction.Start("View Creation");
+                            ElementId titleBlockId = new FilteredElementCollector(doc)
+                                 .OfClass(typeof(FamilySymbol))
+                                 .OfCategory(BuiltInCategory.OST_TitleBlocks)
+                                 .Cast<FamilySymbol>()
+                                 .FirstOrDefault().Id;
+
+       
+                            ViewSheet viewSheet = AssemblyViewUtils.CreateSheet(doc, assemblyInstance.Id, titleBlockId);
+                            View3D view3d = AssemblyViewUtils.Create3DOrthographic(doc, assemblyInstance.Id);
+
+                            //List<Element> elementsInView = new FilteredElementCollector(doc, viewSheet.Id).ToList();
+                            //ViewSchedule viewSchedule = AssemblyViewUtils.CreateSingleCategorySchedule(doc, assemblyInstance.Id);
+
+                            //ViewSection elevationTop = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.ElevationTop);
+                            //ViewSection elevationLeft = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.ElevationLeft);
+                            //ViewSection elevationRight = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.ElevationRight);
+                            //ViewSection elevationFront = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.ElevationFront);
+                            //ViewSection detailSectionA = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.DetailSectionA);
+                            //ViewSection detailSectionB = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.DetailSectionB);
+                            //ViewSection detailSectionH = AssemblyViewUtils.CreateDetailSection(doc, assemblyInstance.Id, AssemblyDetailViewOrientation.HorizontalDetail);
+                            //ViewSchedule materialTakeoff = AssemblyViewUtils.CreateMaterialTakeoff(doc, assemblyInstance.Id);
+                            ViewSchedule partList = AssemblyViewUtils.CreatePartList(doc, assemblyInstance.Id);
+                            ScheduleDefinition partSchedule = partList.Definition;
+                            partSchedule.IsItemized = false;
+
+                            ICollection<ElementId> schedulableFields = partSchedule.GetValidCategoriesForEmbeddedSchedule();
+
+                            //WORK IN PROGRESS
+                            //List<string> schedulableFieldNames = new List<string>();
+                            //foreach (ElementId elementId in schedulableFields)
+                            //{
+                            //    schedulableFieldNames.Add(elementId);
+                            //}
+
+                            Viewport.Create(doc, viewSheet.Id, view3d.Id, new XYZ(1, 1, 0));
+                            //Viewport.Create(doc, viewSheet.Id, elevationTop.Id, new XYZ(2, 2, 0));
+                            //Viewport.Create(doc, viewSheet.Id, elevationLeft.Id, new XYZ(1, 1.7, 0));
+                            //Viewport.Create(doc, viewSheet.Id, elevationRight.Id, new XYZ(2.5, 2, 0));
+                            //Viewport.Create(doc, viewSheet.Id, elevationFront.Id, new XYZ(2, 1, 0));
+                            //Viewport.Create(doc, viewSheet.Id, detailSectionA.Id, new XYZ(1.5, 1.25, 0));
+                            //Viewport.Create(doc, viewSheet.Id, detailSectionB.Id, new XYZ(0.5, 1.5, 0));
+                            //Viewport.Create(doc, viewSheet.Id, detailSectionH.Id, new XYZ(1.5, 2, 0));
+                            //ScheduleSheetInstance.Create(doc, viewSheet.Id, materialTakeoff.Id, new XYZ(2, 2.5, 0));
+                            ScheduleSheetInstance.Create(doc, viewSheet.Id, partList.Id, new XYZ(2.5, 2.5, 0));
+
+                            transaction.Commit();
+                        }
+                    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                }
+
+            }
+
+            return Result.Succeeded;
+
+        }
+    }
+}
---
+++ KGE_Quantifications.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.DB.Plumbing;
+
+namespace API_2021_Plugins
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+    public class KGE_Quantifications : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //Get UI Document
+            UIDocument uidoc = commandData.Application.ActiveUIDocument;
+
+            //Get Document
+            Document doc = uidoc.Document;
+
+            //Create Filtered Element Collector
+            FilteredElementCollector collector1 = new FilteredElementCollector(doc);
+            FilteredElementCollector collector2 = new FilteredElementCollector(doc);
+            FilteredElementCollector collector3 = new FilteredElementCollector(doc);
+
+            //Create Category Filters
+            //ElementCategoryFilter pipesFilter = new ElementCategoryFilter(BuiltInCategory.OST_PipeSegments);
+            //ElementCategoryFilter ductsFilter = new ElementCategoryFilter(BuiltInCategory.OST_DuctCurves);
+            //ElementCategoryFilter traysFilter = new ElementCategoryFilter(BuiltInCategory.OST_CableTray);
+
+            ElementClassFilter pipesFilter = new ElementClassFilter(typeof(Pipe));
+            ElementClassFilter ductsFilter = new ElementClassFilter(typeof(Duct));
+            ElementClassFilter traysFilter = new ElementClassFilter(typeof(CableTray));
+
+            //Apply Filters
+            IList<Element> pipes = collector1.WherePasses(pipesFilter).WhereElementIsNotElementType().ToElements();
+            IList<Element> ducts = collector2.WherePasses(ductsFilter).WhereElementIsNotElementType().ToElements();
+            IList<Element> trays = collector3.WherePasses(traysFilter).WhereElementIsNotElementType().ToElements(); 
+
+            double pipeLength;
+            double pipeTotalLengths = 0;
+
+            foreach (Element element in pipes)
+            {
+                pipeLength = element.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+                pipeTotalLengths += pipeLength;
+            }
+
+            double ductLength;
+            double ductTotalLengths = 0;
+
+            foreach (Element element in ducts)
+            {
+                ductLength = element.get_Parameter(BuiltInParameter.CURVE_ELEM_LENGTH).AsDouble();
+                ductTotalLengths += ductLength;
+            }
+
+            TaskDialog.Show("Quantifications", $"{pipes.Count} pipes in the model\n" +
+                                                $"{ducts.Count} ducts in the model\n" +
+                                                $"{trays.Count} trays in the model\n" +
+                                                $"Total length of pipes {pipeTotalLengths} m\n" +
+                                                $"Total length of ducts {ductTotalLengths} m");
+
+            return Result.Succeeded;
+
+        }
+    }
+}
